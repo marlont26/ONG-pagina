fa7ab2b5e18915fb8e2789b9f2756abbb3e4a1e1
marlont26 <marlonalbertotorresjimenez@gmail.com>
2024-09-09 07:55:43 -0500

    777

--- project ong/config.py
+++ project ong/config.py
@@ -1,7 +1,4 @@
-# config.py
-
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3306/ong'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+mysqlclient://root:2hefbFfDEc-HFd1fhc2DhBCCgh-HCB65@monorail.proxy.rlwy.net:20020/Libreria1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'mysecretkey'
