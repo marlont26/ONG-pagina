47968a2924f5e3eef151433257e4e61d690b6b67
marlont26 <marlonalbertotorresjimenez@gmail.com>
2024-09-09 11:45:57 -0500

    1

--- project ong/app/models/empleado.py
+++ project ong/app/models/empleado.py
@@ -2,14 +2,14 @@ from app import db
 
 class Empleado(db.Model):
     __tablename__ = 'empleado'
-    idEmpleado = db.Column(db.Integer, primary_key= True, autoincrement=True)
+    idEmpleado = db.Column(db.Integer, primary_key=True, autoincrement=True)
     idUsuario = db.Column(db.Integer, db.ForeignKey('usuario.idUsuario'), nullable=False)
-    nombre= db.Column(db.String(255), nullable= True)
-    apellido= db.Column(db.String(255), nullable=True)
+    nombre = db.Column(db.String(255), nullable=True)
+    apellido = db.Column(db.String(255), nullable=True)
     telefono = db.Column(db.String(20))
-    email= db.Column(db.String(255))
-#perro_empleado relacion tabla intermedia = cuidado
-    usuario = db.relationship('Usuario', back_populates='empleado')
+    email = db.Column(db.String(255))
+
+    # Relaciones
+    usuario = db.relationship('Usuario', back_populates='empleados')
     solicitudes_adopcion = db.relationship('SolicitudAdopcion', back_populates='empleado')
     cuidados = db.relationship('Cuidado', back_populates='empleado')
-    #usuario = db.relationship('Usuario', back_populates='empleado', uselist=False)
--- project ong/app/models/usuario.py
+++ project ong/app/models/usuario.py
@@ -1,15 +1,14 @@
 from flask_login import UserMixin
 from app import db
 
-
 class Usuario(db.Model, UserMixin):
     __tablename__ = 'usuario'
-    
     idUsuario = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    telefono = db.Column(db.Integer, nullable=False)
-    cedula = db.Column(db.Integer, nullable=False)
+    fechaRegistro = db.Column(db.DateTime, default=db.func.current_timestamp())
 
+    # Relación con Empleado
+    empleados = db.relationship('Empleado', back_populates='usuario')
 
