a74ff6f8207fa9bcfacaeb5add8e8ebbd37c2bb3
XxSTVNxX <158865834+XxSTVNxX@users.noreply.github.com>
2024-09-14 10:23:01 -0500

    about us (sin terminar)

--- project ong/app/routes/main_routes.py
+++ project ong/app/routes/main_routes.py
@@ -5,3 +5,7 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def home():
     return render_template('home.html')
+
+@bp.route('/about')
+def about():
+    return render_template('/vistasusuario/about.html')
