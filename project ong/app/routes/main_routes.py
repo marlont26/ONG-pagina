a60a3d78f743b45180f24bb14654c776382fcf24
SamDamn2 <samuelavila051@gmail.com>
2024-09-16 08:00:35 -0500

    Url

--- project ong/app/routes/main_routes.py
+++ project ong/app/routes/main_routes.py
@@ -2,18 +2,22 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('main', __name__)
 
-@bp.route('/')
-def home():
-    return render_template('home.html')
-
 @bp.route('/about')
 def about():
     return render_template('/vistasusuario/about.html')
 
 @bp.route('/baseadm')
 def baseadm():
-    return render_template('/bases/baseadm.html')  # Cambia el nombre del archivo según sea necesario
+    return render_template('/bases/baseadm.html')
 
-@bp.route('/baseusu')
+@bp.route('/baseemple')
+def baseemple():
+    return render_template('/bases/baseemple.html')
+
+@bp.route('/basevete')
+def basevete():
+    return render_template('/bases/basevete.html')
+
+@bp.route('/')
 def baseusu():
-    return render_template('/bases/baseusu.html')  # Cambia el nombre del archivo según sea necesario
+    return render_template('/bases/baseusu.html')
