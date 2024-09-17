a47c6aa1cfb79550f4cd55f60e231e25ed5bb1e8
marlont26 <marlonalbertotorresjimenez@gmail.com>
2024-09-17 08:31:54 -0500

    render template

--- project ong/app/routes/empleado_routes.py
+++ project ong/app/routes/empleado_routes.py
@@ -8,7 +8,7 @@ bp = Blueprint('empleado', __name__)
 @bp.route('/empleados')
 def index():
     empleados = Empleado.query.all()
-    return render_template('empleados/home.html', empleado=empleados)
+    return render_template('empleados/index.html', empleado=empleados)
 
 # Ruta para agregar un nuevo empleado   
 @bp.route('/add/empleados', methods=['GET', 'POST'])
