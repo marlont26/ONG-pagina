ed7388d002d1d25666a69ef001aab90857903e6b
SamDamn2 <samuelavila051@gmail.com>
2024-09-26 11:01:13 -0500

    New

--- project ong/app/models/empleado.py
+++ project ong/app/models/empleado.py
@@ -7,10 +7,6 @@ class Empleado(Usuario):
     __tablename__ = 'empleado'
     idEmple = db.Column(db.Integer, db.ForeignKey('usuario.id'), primary_key=True)
 
-    __mapper_args__ = {
-        'polymorphic_identity': 'empleado',
-    }
-
     # Relaciones específicas de Empleado
     solicitudes_adopcion = db.relationship('SolicitudAdopcion', back_populates='empleado')
     cuidados = db.relationship('Cuidado', back_populates='empleado')
--- project ong/app/models/veterinario.py
+++ project ong/app/models/veterinario.py
@@ -1,15 +1,8 @@
 from app import db
-
 from app.models.usuario import Usuario
-from app import db
 
 class Veterinario(Usuario):
     __tablename__ = 'veterinario'
     idVete = db.Column(db.Integer, db.ForeignKey('usuario.id'), primary_key=True)
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'veterinario',
-    }
-
     # Relaciones específicas de Veterinario
     visitas_medicas = db.relationship('VisitaMedica', back_populates='veterinario')
