5d93edc9881e9db7df7837ab91c558fb742df2de
SamDamn2 <samuelavila051@gmail.com>
2024-09-29 12:48:35 -0500

    Local

--- project ong/config.py
+++ project ong/config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:VGMjFYjIDXGoTpGefwJnIekxtJeMdNvl@mysql.railway.internal:3306/ong'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost:3306/ong'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'mysecretkey'
