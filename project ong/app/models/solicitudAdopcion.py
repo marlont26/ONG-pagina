2a11005eb6a38c9252c4f2155e4ab151bba8b342
marlont26 <marlonalbertotorresjimenez@gmail.com>
2024-09-30 02:48:59 -0500

    solicitud

--- project ong/app/models/solicitudAdopcion.py
+++ project ong/app/models/solicitudAdopcion.py
@@ -2,14 +2,11 @@ from app import db
 
 class SolicitudAdopcion(db.Model):
     __tablename__ = 'solicitud_adopcion'
-    idSolicitud = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     idPerro = db.Column(db.Integer, db.ForeignKey('perro.idPerro'))
-    idAdoptante = db.Column(db.Integer, db.ForeignKey('usuario.id', ondelete='CASCADE'), nullable=False)
-    fechaSolicitud = db.Column(db.Date)
-    estado = db.Column(db.String(255))
-    idEmpleado = db.Column(db.Integer, db.ForeignKey('empleado.idEmple'))  # Asegúrate de que 'idEmpleado' exista en 'empleado'
-    comentarios = db.Column(db.String(255))
+    idUsuario = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    fecha = db.Column(db.Date)
+    estado = db.Column(db.String(50), default='pendiente')  # Nuevo campo para el estado
     
     perro = db.relationship('Perro', back_populates='solicitudes_adopcion')
-    empleado = db.relationship('Empleado', back_populates='solicitudes_adopcion', foreign_keys=[idEmpleado])
-    adoptante = db.relationship('Usuario', back_populates='solicitudes_adopcion')
+    usuario = db.relationship('Usuario', back_populates='solicitudes_adopcion')
