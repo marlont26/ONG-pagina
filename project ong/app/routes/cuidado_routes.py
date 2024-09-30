2cba5e1ac0739f6f2178ab8b2a0d23d44d7a9499
SamDamn2 <samuelavila051@gmail.com>
2024-09-29 23:24:09 -0500

    New

--- project ong/app/routes/cuidado_routes.py
+++ project ong/app/routes/cuidado_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from app.models import Cuidado, Empleado, Perro
 from app import db
 
