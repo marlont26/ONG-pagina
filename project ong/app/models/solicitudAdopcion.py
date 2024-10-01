80737ae92e892cadf12e77f8c7dc273ed6b99f80
XxSTVNxX <158865834+XxSTVNxX@users.noreply.github.com>
2024-10-01 10:06:16 -0500

    test

--- project ong/app/models/solicitudAdopcion.py
+++ project ong/app/models/solicitudAdopcion.py
@@ -2,11 +2,12 @@ from app import db
 
 class SolicitudAdopcion(db.Model):
     __tablename__ = 'solicitud_adopcion'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    idSolicitud = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    idAdoptante = db.Column(db.Integer, db.ForeignKey('usuario.id'))
     idPerro = db.Column(db.Integer, db.ForeignKey('perro.idPerro'))
-    idUsuario = db.Column(db.Integer, db.ForeignKey('usuario.id'))
-    fecha = db.Column(db.Date)
-    estado = db.Column(db.String(50), default='pendiente')  # Nuevo campo para el estado
+    estado = db.Column(db.String(50))
     
+    # Relaciones
+    adoptante = db.relationship('Usuario', back_populates='solicitudes_adopcion')
     perro = db.relationship('Perro', back_populates='solicitudes_adopcion')
-    usuario = db.relationship('Usuario', back_populates='solicitudes_adopcion')
+    empleado = db.relationship('Empleado', back_populates='solicitudes_adopcion')
---
+++ project ong/app/test/test_users.py
@@ -0,0 +1,25 @@
+def test_index(client):
+    response = client.get('/Usuario/')
+    assert response.status_code == 200
+    assert b"Usuarios Registrados" in response.data  # Asumiendo que hay un texto "Lista de Usuarios" en la página
+
+def test_add_user(client):
+    response = client.post('/Usuario/add', data={
+        'nombre': 'new_user',
+        'password': 'new_password'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Usuarios" in response.data  # Asumiendo que el nombre del usuario se muestra en la página
+
+def test_edit_user(client, usuario):
+    response = client.post(f'/Usuario/edit/{usuario.id}', data={
+        'nombre': 'updated_user',
+        'password': 'updated_password'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    assert b"updated_user" in response.data  # Asumiendo que el nombre del usuario actualizado se muestra en la página
+
+def test_delete_user(client, usuario):
+    response = client.get(f'/Usuario/delete/{usuario.id}', follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Usuarios" in response.data  # Asumiendo que hay un mensaje de confirmación de eliminación
